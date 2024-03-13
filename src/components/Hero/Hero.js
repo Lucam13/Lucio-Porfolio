cea17d6ff1fb8564e37eda6a0717278128170a1d
Lucam13 <lucc_88@hotmail.com>
2024-03-13 15:53:36 -0300

    Initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+import { FiverUrl, ResumeUrl, UpworkUrl } from "../../constants/constants";
+
+const handleClickFiverr = () => {
+  window.open(FiverUrl, "_blank");
+  
+};
+const handleClickUpwork = () => {
+  window.open(UpworkUrl, "_blank");
+};
+const handleClickResume = () => {
+  window.open(ResumeUrl, "_blank");
+};
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hello! <br />
+          I'm Lucio 
+        </SectionTitle>
+        <SectionText>
+          A Full Stack Developer specializing in building and designing
+          applications with Node.js. Proficient in Next.js and React.js for
+          seamless web development.
+        </SectionText>
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" ,margin: "0 0 80px" }}>
+          <Button alt="upwork" onClick={handleClickUpwork}>
+            Upwork
+          </Button>
+          {/* <Button alt="fiverr" onClick={handleClickFiverr}>
+            Fiverr
+          </Button> */}
+          <Button alt="resume" onClick={handleClickResume}>
+            Resume
+          </Button>
+        </div>
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,76 @@
+export const projects = [
+  {
+    title: "Workaway",
+    description:
+      "Work Away es una plataforma innovadora que permite a los profesionales y empresas encontrar y reservar espacios de trabajo remoto en todo el mundo. Con una amplia gama de características y tecnologías modernas.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/workaway-91787.appspot.com/o/workaway.jpg?alt=media&token=cd22622e-ca90-48c4-a582-f1e4d94a0700",
+    // image: "/images/jp01.jpg",
+    tags: ["Node.js", "Prisma", "Flutter", "GraphQL", "Pubsub"],
+
+    source:
+      "https://play.google.com/store/apps/details?id=com.thepartycade.partycade",
+    visit: null,
+    id: 0,
+  },
+  {
+    title: "Hangman",
+    description:
+      "A hangman game made with python3. The game is a simple word guessing game. The player has to guess the word by suggesting letters within a certain number of guesses.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/hangman-b0fcc.appspot.com/o/hangman.jpg?alt=media&token=02738757-0162-4680-a62d-eb4b4bccbce0", 
+      
+    // image: "/images/jp01.jpg",
+    tags: ["python3"],
+
+    source:
+      "https://workaway.vercel.app/",
+    visit: null,
+
+    id: 1,
+  },
+  {
+  title: "Workaway",
+    description:
+      "Work Away es una plataforma innovadora que permite a los profesionales y empresas encontrar y reservar espacios de trabajo remoto en todo el mundo. Con una amplia gama de características y tecnologías modernas, Work Away ofrece una experiencia fluida y conveniente para aquellos que buscan un entorno de trabajo productivo fuera de la oficina tradicional.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/workaway-91787.appspot.com/o/workaway.jpg?alt=media&token=cd22622e-ca90-48c4-a582-f1e4d94a0700",
+    // image: "/images/jp01.jpg",
+    tags: ["Node.js", "Prisma", "Flutter", "GraphQL", "Pubsub"],
+
+    source:
+      "https://play.google.com/store/apps/details?id=com.thepartycade.partycade",
+    visit: null,
+    id: 0,
+  },
+  {
+    title: "Workaway",
+      description:
+        "The Partycade offers unique games for many occasions. Such as Birthday Dinners, Baby Showers, Bridal Showers and many more. You’ll get to enjoy a friendly fun competition with your loved ones all from your smart device. Can’t make it to the event? No worries, you can join the game room wherever you are.",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/workaway-91787.appspot.com/o/workaway.jpg?alt=media&token=cd22622e-ca90-48c4-a582-f1e4d94a0700",
+      // image: "/images/jp01.jpg",
+      tags: ["Node.js", "Prisma", "Flutter", "GraphQL", "Pubsub"],
+  
+      source:
+        "https://play.google.com/store/apps/details?id=com.thepartycade.partycade",
+      visit: null,
+      id: 0,
+    },
+  
+  
+];
+
+
+
+export const FiverUrl = "https://www.fiverr.com/";
+export const UpworkUrl =
+  "https://www.upwork.com/freelancers/~01e171a46da5cc447d";
+export const ResumeUrl = "";
+export const GithubUrl = "";
+export const LinkedInUrl =
+  "https://www.linkedin.com/in/";
+export const FacebookUrl =
+  "https://www.facebook.com/";
+export const MobileNumber = "3541527770";
+export const PersonalEmail = "luciocampos1388@gmail.com";
